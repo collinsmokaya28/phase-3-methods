36891078d9e8f3ff710e0fa9c9dae2acc9563909
+ <+>
2022-10-17 19:41:26 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+# greet
+def greet name
+    puts "Hello, #{name}!"
+end
+
+#default
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+# add
+
+def add num1, num2
+    puts num1
+    puts num2
+    return num1 + num2
+end
+
+# halve
+
+def halve num
+    if num.class != Integer
+       return nil
+    end
+    num / 2
+end
